6e5fe59d20384e57a9fb209f8a7e5e203ec5293d
sabri-71 <salrbizi@gmail.com>
2025-11-24 20:55:06 +0300

    first upload

--- android/app/src/main/java/com/adenrates/currencyapp/MainActivity.java
+++ android/app/src/main/java/com/adenrates/currencyapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.adenrates.currencyapp;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.unity3d.ads.UnityAds;
 
 public class MainActivity extends BridgeActivity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Register the UnityAds plugin so JS can call native methods
-		registerPlugin(UnityAdsPlugin.class);
-	}
+    String unityGameID = "5967793";
+    Boolean testMode = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Register the UnityAds plugin so JS can call native methods
+        registerPlugin(UnityAdsPlugin.class);
+        // Initialize the Unity Ads SDK
+        UnityAds.initialize(getApplicationContext(), unityGameID, testMode);
+    }
 }
